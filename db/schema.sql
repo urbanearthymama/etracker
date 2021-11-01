ff790a8924a3a1130c74efde13090dd4ed39cdc8
Patricia Nelson <urbanearthymama@gmail.com>
2021-10-31 23:27:27 -0500

    tables added to schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,15 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
+);
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
